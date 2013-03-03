ee156c54643ac9b3716a92e9b669f9d59f52a34a
daxofdeath <daxdefranco+git@gmail.com>
2013-03-03 11:24:01 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeacherApp::Application.config.secret_token = 'ef75919985241b3dfb7bd16cbfb79aa833de76aea38f0594e3f38c0ecbc49c91839d36a15a4c4f83b8b9cff0261132cba87d1d3f23b03a4647b7e6607bd8d5fa'
