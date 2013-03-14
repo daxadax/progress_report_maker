b893171089b41ef3dfc6fd528cba21687b5c6949
daxofdeath <daxdefranco+git@gmail.com>
2013-03-14 22:18:52 +0100

    added Student model

---
+++ app/models/student.rb
@@ -0,0 +1,3 @@
+class Student < ActiveRecord::Base
+  attr_accessible :gender, :name, :notes
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314204426) do
+ActiveRecord::Schema.define(:version => 20130314211814) do
 
   create_table "student_groups", :force => true do |t|
     t.string   "name"
     t.string   "subject"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "students", :force => true do |t|
+    t.string   "name"
+    t.string   "gender"
+    t.text     "notes"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
