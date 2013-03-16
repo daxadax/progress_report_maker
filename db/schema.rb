15e32b832b1beaceb473872647fa8fb32d25d9b8
daxofdeath <daxdefranco+git@gmail.com>
2013-03-16 09:10:00 +0100

    development database issue?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314211814) do
+ActiveRecord::Schema.define(:version => 20130315110054) do
 
   create_table "student_groups", :force => true do |t|
     t.string   "name"
