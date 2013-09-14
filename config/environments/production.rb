900097fbf7ea12fd893594d19d41beb539dab6c9
daxdefranco <daxdefranco+git@gmail.com>
2013-09-14 15:31:51 +0200

    changed logging level to :debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ config.assets.precompile += %w( .svg .eot .woff .ttf *.png
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
