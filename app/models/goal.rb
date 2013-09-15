4b2bfed0be0ece57b02c7e8264580105da327606
daxdefranco <daxdefranco+git@gmail.com>
2013-09-15 10:16:33 +0200

    testing error

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -11,7 +11,7 @@
 #
 
 class Goal < ActiveRecord::Base
-  include ::Averageable
+  # include Averageable
   
   attr_accessible :goal
   
@@ -23,6 +23,12 @@ class Goal < ActiveRecord::Base
   before_save { |g| g.goal = goal.humanize }
   
   # methods
+  
+  def average(scores)
+    # get average of scores and round to two decimal places
+    average = scores.inject{ |sum, el| sum + el }.to_f / scores.size
+    average.round(2)
+  end
   
   # http://stackoverflow.com/a/1341318/2128691
   def avg
